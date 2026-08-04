65611305790536bd911110d4ee941fb90ebf20ed
Tiago Hierath <tiagohierath@gmail.com>
2026-08-04 17:57:32 -0300

    update nixos

--- packages.nix
+++ packages.nix
@@ -38,7 +38,7 @@ with pkgs; [
   fzf
   ollama
   cool-retro-term
-  wine64
+  bottles
   
   # ── Moved here from home-manager (2026-07-10) ──────────────────────────────
   # Wayland / Hyprland ecosystem
